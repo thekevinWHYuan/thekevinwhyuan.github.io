0e2dd81a8c2df60fcf3ad02105bafff99b009d14
Kevin <kev.yuan01@gmail.com>
2026-02-12 19:44:42 -0800

    under renovations

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -6,19 +6,23 @@ const Landing: Component = () => {
 
     const { store } = useWindowsContext();
     
-    return (<div class="w-screen h-screen fixed">
+    // return (<div class="w-screen h-screen fixed">
 
-        <div>
-            <Show when={store.scanline}>
-                <div class="w-screen h-screen bg-mini-scanline opacity-15 absolute z-10 pointer-events-none"/>
-                <div class="w-full absolute h-[10%] bg-gray-200 opacity-30 pointer-events-none animate-scanline z-20"/>
-            </Show>
-            <div class="w-inherit min-h-[100svh] flex justify-center items-center">
-                <WindowsScreen/>
-            </div>
-        </div>
+    //     <div>
+    //         <Show when={store.scanline}>
+    //             <div class="w-screen h-screen bg-mini-scanline opacity-15 absolute z-10 pointer-events-none"/>
+    //             <div class="w-full absolute h-[10%] bg-gray-200 opacity-30 pointer-events-none animate-scanline z-20"/>
+    //         </Show>
+    //         <div class="w-inherit min-h-[100svh] flex justify-center items-center">
+    //             <WindowsScreen/>
+    //         </div>
+    //     </div>
 
-    </div>);
+    // </div>);
+
+    return (<div class="flex w-screen h-screen items-center justify-center">
+        <h1 class="text-2xl">Website under renovations !!</h1>
+    </div>)
 
 }   
 
